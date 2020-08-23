e0f1c7075ba3be1a633b435f07b276456024dc07
Einar Gustafsson <einargustafsson@users.noreply.github.com>
2020-08-23 13:44:57 +0000

    adding netlify cms

--- pages/index.vue
+++ pages/index.vue
@@ -10,6 +10,11 @@ import VmProductsList from '@/components/products_list/ProductsListContainer';
 
 
 export default {
+  head() {
+    return {
+      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
+    };
+  },
   name: 'index',
   components: {
     VmProductsList
