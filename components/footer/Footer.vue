2369d66a8591fa281f46773b2007f18449584e61
Einar Gustafsson <einargustafsson@users.noreply.github.com>
2020-08-24 16:05:54 +0000

    review links in footer

--- components/footer/Footer.vue
+++ components/footer/Footer.vue
@@ -3,7 +3,12 @@
     <div class="columns container">
       
     <div class="column has-text-center">
-      <p>Get a touch free mobile menu on MyMenu.is</p>
+      <h2>Please leave a review</h2>
+      <ul>
+        <li><a href="https://www.tripadvisor.com/Restaurant_Review-g189970-d1960952-Reviews-Cafe_Loki-Reykjavik_Capital_Region.html" target="_blank">Tripadvisor</a></li>
+        <li><a href="https://facebook.com/cafeloki" target="_blank">Facebook</a></li>
+        </ul>
+      
     </div>
     </div>
   </div>
@@ -20,4 +25,17 @@ export default {
    background: #209cee;
    color: #fff;
  }
+ .footer a {
+    color: #fff; 
+    text-decoration: underline;
+}
+.footer li {display: inline-block; margin-right: 20px;}
+.footer ul {
+    margin-bottom: 40px;
+}
+
+.footer h2 {
+    font-size: 20px;
+    font-weight: bold;
+}
 </style>
